270807a424232627a9b89d234f31ecbe5a558fe5
nascimentooliveira-lab <nascimento.oliveira@aluno.ufca.edu.br>
2026-03-18 15:44:44 -0300

    alterações no menu e no crud

--- src/database/create.py
+++ src/database/create.py
@@ -1,24 +1,67 @@
 from src.database.conexao import conectar
 
 # ---------------- CREATE ----------------
-def inserir_endereco(id_endereco, logradouro, cep, rua, numero):
+
+def inserir_endereco(id_endereco, cep, rua, numero, bairro):
     conn = conectar()
     cursor = conn.cursor()
-    sql = """INSERT INTO ENDERECO (ID_ENDERECO, LOGRADOURO, CEP, RUA, NUMERO)
-             VALUES (%s, %s, %s, %s, %s)"""
-    cursor.execute(sql, (id_endereco, logradouro, cep, rua, numero))
+    sql_endereco = """INSERT INTO endereco (CEP, RUA, NUMERO, BAIRRO)
+                  VALUES (%s, %s, %s, %s)"""
+    cursor.execute(sql_endereco, (cep, rua, numero, bairro))
     conn.commit()
     print("Endereço inserido com sucesso!")
     conn.close()
 
-def inserir_pessoa_com_endereco(cpf, nome, data_nascimento, logradouro, cep, rua, numero):
+def inserir_oferece(id_unidade, id_servico, dia_semana, hora_inicio, hora_final, vagas):
+    conn = conectar()
+    cursor = conn.cursor()
+    sql = """INSERT INTO oferece (ID_UNIDADE, ID_SERVICO, DIA_SEMANA, HORA_INICIO, HORA_FINAL, VAGAS)
+             VALUES (%s, %s, %s, %s, %s, %s)"""
+    cursor.execute(sql, (id_unidade, id_servico, dia_semana, hora_inicio, hora_final, vagas))
+    conn.commit()
+    print("Oferta de serviço inserida com sucesso!")
+    conn.close()
+
+def inserir_unidade(nome, tipo, cep, rua, numero, bairro):
     conn = conectar()
     cursor = conn.cursor()
 
-    # Inserir endereço
-    sql_endereco = """INSERT INTO ENDERECO (LOGRADOURO, CEP, RUA, NUMERO)
+    # Primeiro insere o endereço
+    sql_endereco = """INSERT INTO endereco (CEP, RUA, NUMERO, BAIRRO)
                       VALUES (%s, %s, %s, %s)"""
-    cursor.execute(sql_endereco, (logradouro, cep, rua, numero))
+    cursor.execute(sql_endereco, (cep, rua, numero, bairro))
+    conn.commit()
+    id_endereco = cursor.lastrowid  # pega o ID do endereço recém-criado
+
+    # Depois insere a unidade vinculada ao endereço
+    sql_unidade = """INSERT INTO unidade (NOME, TIPO, ID_ENDERECO)
+                     VALUES (%s, %s, %s)"""
+    cursor.execute(sql_unidade, (nome, tipo, id_endereco))
+    conn.commit()
+
+    print("Unidade e endereço inseridos com sucesso!")
+    conn.close()
+
+          
+def inserir_servico(nome, tempo_medio):
+    conn = conectar()
+    cursor = conn.cursor()
+    sql = "INSERT INTO servico (NOME, TEMPO_MEDIO) VALUES (%s, %s)"
+    cursor.execute(sql, (nome, tempo_medio))
+    conn.commit()
+    print("Serviço inserido com sucesso!")
+    conn.close()
+
+def inserir_pessoa_com_endereco(cpf, nome, data_nascimento, cep, rua, numero, bairro):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    sql_endereco = """INSERT INTO ENDERECO ( CEP, RUA, NUMERO, BAIRRO)
+                  VALUES (%s, %s, %s, %s)"""
+    
+    cursor.execute(sql_endereco, (cep, rua, numero, bairro))
+
+    cursor.execute(sql_endereco, (cep, rua, numero, bairro))
     id_endereco = cursor.lastrowid
 
     # Inserir pessoa vinculada ao endereço
@@ -30,20 +73,67 @@ def inserir_pessoa_com_endereco(cpf, nome, data_nascimento, logradouro, cep, rua
     print("Pessoa e endereço inseridos com sucesso!")
     conn.close()
 
-def inserir_servico(nome, tempo_medio):
+
+def inserir_usuario(id_pessoa, email, senha_hash, perfil):
     conn = conectar()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO Servico (NOME, TEMPO_MEDIO) VALUES (%s, %s)", (nome, tempo_medio))
+    sql = """INSERT INTO usuario (ID_PESSOA, EMAIL, SENHA_HASH, PERFIL)
+             VALUES (%s, %s, %s, %s)"""
+    cursor.execute(sql, (id_pessoa, email, senha_hash, perfil))
     conn.commit()
-    print("Serviço inserido com sucesso!")
+    print("Usuário inserido com sucesso!")
     conn.close()
 
-def inserir_oferece(id_unidade, id_servico, dia_semana, hora_inicio, hora_final, vagas):
+    
+def verificar_pessoa_existe(cpf):
     conn = conectar()
     cursor = conn.cursor()
-    sql = """INSERT INTO Oferece (ID_UNIDADE, ID_SERVICO, DIA_SEMANA, HORA_INICIO, HORA_FINAL, VAGAS)
-             VALUES (%s, %s, %s, %s, %s, %s)"""
-    cursor.execute(sql, (id_unidade, id_servico, dia_semana, hora_inicio, hora_final, vagas))
+    cursor.execute("SELECT 1 FROM pessoa WHERE CPF = %s", (cpf,))
+    existe = cursor.fetchone() is not None
+    conn.close()
+    return existe
+
+def inserir_funcionario(id_pessoa, matricula, cargo, admissao):
+    conn = conectar()
+    cursor = conn.cursor()
+    sql = """INSERT INTO funcionario (ID_PESSOA, MATRICULA, CARGO, ADMISSAO)
+             VALUES (%s, %s, %s, %s)"""
+    cursor.execute(sql, (id_pessoa, matricula, cargo, admissao))
     conn.commit()
-    print("Associação Unidade-Serviço inserida com sucesso!")
+    print("Funcionário inserido com sucesso!")
+    conn.close()
+
+def inserir_cidadao(id_pessoa, cartao_sus, nis):
+    conn = conectar()
+    cursor = conn.cursor()
+    sql = """INSERT INTO cidadao (ID_PESSOA, CARTAO_SUS, NIS)
+             VALUES (%s, %s, %s)"""
+    cursor.execute(sql, (id_pessoa, cartao_sus, nis))
+    conn.commit()
+    print("Cidadão inserido com sucesso!")
+    conn.close()
+
+
+def inserir_agendamento(observacao, data, hora,
+                        data_inicio_realizado, data_final_realizado, horario_realizado,
+                        id_funcionario_realizado,
+                        id_unidade_sediada, id_cidadao_solicitado, id_servico_referente,
+                        status_agendamento, prioridade, notificacao):
+    conn = conectar()
+    cursor = conn.cursor()
+    sql = """INSERT INTO agendamento (
+                OBSERVACAO, DATA, HORA,
+                DATA_INICIO_REALIZADO, DATA_FINAL_REALIZADO, HORARIO_REALIZADO,
+                ID_FUNCIONARIO_REALIZADO, ID_UNIDADE_SEDIADA, ID_CIDADAO_SOLICITADO,
+                ID_SERVICO_REFERENTE, STATUS_AGENDAMENTO, PRIORIDADE, NOTIFICACAO
+             )
+             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+    cursor.execute(sql, (
+        observacao, data, hora,
+        data_inicio_realizado, data_final_realizado, horario_realizado,
+        id_funcionario_realizado, id_unidade_sediada, id_cidadao_solicitado,
+        id_servico_referente, status_agendamento, prioridade, notificacao
+    ))
+    conn.commit()
+    print("Agendamento inserido com sucesso!")
     conn.close()
